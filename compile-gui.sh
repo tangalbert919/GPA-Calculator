5fb33eb2e9f18bd11dc1f07d9e86d5552d365ed2
Albert Tang <tangalbert919@gmail.com>
2017-05-02 10:55:00 -0500

    Fix script file

--- compile-gui.sh
+++ compile-gui.sh
@@ -4,13 +4,13 @@ echo The GUI version of the GPA Calculator will now be compiled.
 
 echo Please wait while we compile the files...
 
-javac src/io/github/tangalbert919/Main2.java src/io/github/tangalbert919/IOFile.java javac src/io/github/tangalbert919/GPACalcGUI.java
+javac src/io/github/tangalbert919/Main2.java src/io/github/tangalbert919/IOFile.java src/io/github/tangalbert919/GPACalcGUI.java
 
 echo Compilation succeeded. Building the jar file now...
 
 cd src
 
-jar cvfm ../GPA-Calculator.jar ../Manifest/manifest2.txt io/github/tangalbert919/*.class
+jar cvfm ../GPA-Calculator.jar ../Manifest/manifest2.txt ./io/github/tangalbert919/*.class
 
 echo Build complete. Please run it by using the run shell script \(run.sh\).
 
